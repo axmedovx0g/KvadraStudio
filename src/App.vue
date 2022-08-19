4d63ba300e892485436b263c80e8d17571e60908
winco6104 <axmedovx0g@gmail.com>
2022-08-19 17:48:34 +0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="app">
+    <home></home>
     <router-view />
   </div>
 </template>
-
+<script>
+import home from '@/views/HomeView.vue'
+export default {
+  components: {
+    home
+  },
+};
+</script>
 <style>
-*{
+* {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
